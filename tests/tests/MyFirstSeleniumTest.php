f6ed51069e45a1c498b9ec1d842e65d506795109
Petter Blomberg <git@petterblomberg.se>
2017-04-22 15:46:15 +0200

    MyFirstSeleniumTest: fix code style

--- tests/tests/MyFirstSeleniumTest.php
+++ tests/tests/MyFirstSeleniumTest.php
@@ -1,16 +1,12 @@
 <?php
-class MyFirstSeleniumTest extends PHPUnit_Extensions_Selenium2TestCase
-{
-    protected function setUp()
-    {
-        $this->setBrowser('firefox');
-        $this->setBrowserUrl('http://localhost/');
-    }
+class MyFirstSeleniumTest extends PHPUnit_Extensions_Selenium2TestCase {
+	protected function setUp() {
+		$this->setBrowser('firefox');
+		$this->setBrowserUrl('http://localhost/');
+	}
 
-    public function testTitle()
-    {
-        $this->url('http://localhost/');
-        $this->assertEquals('Domain Overview', $this->title());
-    }
-
+	public function testTitle() {
+		$this->url('http://localhost/');
+		$this->assertEquals('Domain Overview', $this->title());
+	}
 }
